c002878173861c870957c792c13b1f4fb7a7591c
Bachir Bekkaye. <bachirbk@gmail.com>
2024-05-26 04:36:25 -0700

    Add project files.

---
+++ CustomRevitAPI/TestCommands/SelectionCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CustomRevitAPI.TestCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class SelectionCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiApplication = commandData.Application;
+            var application = uiApplication.Application;
+            var uiDocument = uiApplication.ActiveUIDocument;
+            var document = uiDocument.Document;
+            var collector = new FilteredElementCollector(document)
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .ToElements();
+            TaskDialog.Show("Message", collector.Count.ToString());
+
+            return Result.Succeeded;
+
+        }
+    }
+}
